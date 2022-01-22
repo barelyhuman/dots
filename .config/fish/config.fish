6bea2fc7c7d94349810d974f1946b25428798386
Reaper <ahoy@barelyhuman.dev>
2022-01-23 04:09:24 +0530

    updated greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,17 +2,21 @@ fish_add_path /opt/homebrew/bin
 source ~/.aliases
 
 if status is-interactive
-
-    set fish_greeting "On duty, 💀 Sir."
-    
+	
+    set fish_greeting "At your service sir."
     use-terminal-colors
+    set --global hydro_color_prompt yellow
+    set --global hydro_color_pwd yellow
+    
+    
 
-    function fish_prompt
-        set -g fish_prompt_pwd_dir_length 0
-        printf '%s%s> ' (prompt_pwd) (set_color yellow; fish_git_prompt; set_color normal)
-    end
+    fish_prompt
+
 end
 
 
 
 abbr --add .git "git --git-dir=$HOME/dots.git --work-tree=$HOME"
+
+set -gx PNPM_HOME "/Users/sid/Library/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
